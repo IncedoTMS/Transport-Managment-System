4da459bdda799a38be36fc3507604aa33bd1f6b1
Gaurav Nirula <gaurav.nirula@incedoinc.com>
2022-07-29 18:30:41 +0530

    Defined Components and Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Dashboard from "./dashboard/Dashboard";
+import Signin from "./singin/Signin";
+import Signup from "./signup/Signup";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        {/*
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="/">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/signin">Signin</a>
+                </li>
+                <li class="nav-item dropdown">
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="./signup" aria-disabled="false">Signup</a>
+                </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search"/>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+            </div>
+          </div>
+        </nav> */
+
+          //********************* This part of the code is a <NAVBAR> which is still in progress.*****************//
+        }
+        
+        { /* These Buttons below are routing links to components */}
+        <Link to="/">{<button type="button" class="btn btn-outline-primary">Dashboard</button>}</Link>
+        <Link to="/signin">{<button type="button" class="btn btn-outline-primary">Signin</button>}</Link>
+        <Link to="/signup">{<button type="button" class="btn btn-outline-primary">Signup</button>}</Link>
+
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/signin" component={Signin} />
+        <Route path="/signup" component={Signup} />
+
+      </Router>
+
+      {/* <Switch>
+      <Route exact path='/'>
+        <Dashboard/>
+      </Route>
+      <Route path='/signup'>
+        <Signup/>                     //==> Testig Routing Algorithm
+      </Route>
+      <Route path='/signin'>
+        <Signin/>
+      </Route>
+    </Switch> */}
+
+
+
+    </>
   );
 }
 
---
+++ src/signup/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const Signup = () => {
+    return(
+        <>
+        <h1>Singup Page</h1>
+        </>
+    )
+}
+
+export default Signup;
