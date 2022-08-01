4c8832ab0244bf8e90f1055f29b22a29a7a5887c
Gaurav Nirula <gaurav.nirula@incedoinc.com>
2022-08-01 18:24:35 +0530

    Removed Navbar & Modified Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,46 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Dashboard from "./dashboard/Dashboard";
-import Signin from "./singin/Signin";
-import Signup from "./signup/Signup";
+import Dashboard from "./components/Dashboard";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
 
 
 function App() {
   return (
     <>
       <Router>
-        {/*
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="/">Dashboard</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/signin">Signin</a>
-                </li>
-                <li class="nav-item dropdown">
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="./signup" aria-disabled="false">Signup</a>
-                </li>
-              </ul>
-              <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search"/>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-            </div>
-          </div>
-        </nav> */
 
-          //********************* This part of the code is a <NAVBAR> which is still in progress.*****************//
-        }
-        
         { /* These Buttons below are routing links to components */}
         <Link to="/">{<button type="button" class="btn btn-outline-primary">Dashboard</button>}</Link>
         <Link to="/signin">{<button type="button" class="btn btn-outline-primary">Signin</button>}</Link>
