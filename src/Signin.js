5dbd369690e3faef496dc3bbd510750871567ca7
himanshi-incedo <himanshi.sharma1@incedoinc.com>
2022-08-08 17:48:12 +0530

    Added-Helmet-to change title bar.solved-zoom

--- src/Signin.js
+++ src/Signin.js
@@ -1,44 +1,62 @@
-import React from 'react';
-import './Signin.css';
+import React from "react";
+import { Helmet } from "react-helmet"; //React Helmet use to Dynamically set what's in the document's head section.
+import "./Signin.css";
 
 export default function Signin() {
   return (
     <>
+      <Helmet>
+        <title>Incedo-TMS-SignIn</title>
+        {/*Changes the Title bar of the current page to-Incedo-TMS-SignIn */}
+      </Helmet>
+
       <div class="row">
         <div class="column one col-sm">
           <h2>Image Here</h2>
         </div>
         <div class="column two col-sm">
           <div class="formbox">
-            
             <div class="welcome d-flex justify-content-around">
-              <h3 class='sign-in-h3'>Sign In</h3>
-              <p class='register-link'>New to Transportation Hub? <a href="#">Register</a></p>
+              <h3 class="sign-in-h3">Sign In</h3>
+              <p class="register-link">
+                New to Transportation Hub? <a href="#">Register</a>
+              </p>
             </div>
-            
+
             <form>
               <div class="form-content">
                 <div class="mb-3">
                   <label class="form-label">Email address</label>
-                  <input type="email" class="form-control"  aria-describedby="emailHelp" placeholder='example@gmail.com' />
+                  <input
+                    type="email"
+                    class="form-control"
+                    aria-describedby="emailHelp"
+                    placeholder="example@gmail.com"
+                  />
                 </div>
                 <div class="mb-3">
                   <label class="form-label">Password</label>
-                  <input type="password" class="form-control" placeholder='Password' />
+                  <input
+                    type="password"
+                    class="form-control"
+                    placeholder="Password"
+                  />
                 </div>
               </div>
             </form>
-            
-            <div class='d-flex justify-content-end forget-password'>
-              <a href='#'>Forget Password</a>
+
+            <div class="d-flex justify-content-end forget-password">
+              <a href="#">Forget Password</a>
             </div>
 
-            <div class='d-flex justify-content-center'>
-              <button type="submit" class="btn btn-primary signin-button">Sign-In</button>
-            </div> 
+            <div class="d-flex justify-content-center">
+              <button type="submit" class="btn btn-primary signin-button">
+                Sign-In
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,38 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, useHistory } from "react-router-dom";
-import Dashboard from './Dashboard';
+import React from "react";
+import { Helmet } from "react-helmet"; //React Helmet use to Dynamically set what's in the document's head section.
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  useHistory,
+} from "react-router-dom";
+import Dashboard from "./Dashboard";
 
-import Signup from './Signup';
-
-
+import Signup from "./Signup";
 
 export default function Signin() {
+  const History = useHistory();
+  const routeToSignup = () => {
+    History.push("/signup");
+  };
 
-    const History = useHistory();
-    const routeToSignup = () => {
+  const toDashBoard = () => {
+    History.push("/dashboard");
+  };
 
-
-        History.push('/signup');
-    };
-
-    const toDashBoard = () => {
-        History.push('/dashboard');
-    }
-
-
-    return (
-        <div>Signin
-
-
-            <button onClick={routeToSignup}>
-                Signup
-            </button>
-
-            <button onClick={toDashBoard}>
-                Signin
-            </button>
-
-        </div>
-    )
+  return (
+    <>
+      <Helmet>
+        <title>Incedo-TMS-SignIn</title>
+        {/*Changes the Title bar of the current page to-Incedo-TMS-SignIn */}
+      </Helmet>
+      <div>
+        Signin
+        <button onClick={routeToSignup}>Signup</button>
+        <button onClick={toDashBoard}>Signin</button>
+      </div>
+    </>
+  );
 }
-
