e9baa3dd4ba97c65f535757885ec7eca4e58dee6
Abbas Savliwala <abbas.savliwala@incedoinc.com>
2022-08-09 17:52:48 +0530

    Admin Dashboard: Fixed JSON API async bug

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,56 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Admin.css';
-import { tableData } from './get_tableData';
+import tableDataMethod  from './get_tableData';
 import Table from './CreateTable';
 
 
 export default function Admin() {
-  return (
-    <>
-        <div class='container'>
-            <h4>Admin Page</h4>
-            <Table tableData={tableData}/>
-            
-            {/* <table class="table table-striped table-bordered">
-                <thead class="custom-header">
-                    <tr>
-                        <th scope="col">User ID</th>
-                        <th scope="col">Emp ID</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Role</th>
-                        <th scope="col">Request Date</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Confirm</th>
-                        <th scope="col">Edit</th>
-                    </tr>
-                </thead>
-                <tbody> 
-                    <tr>
-                        <td class='user-id'>0001</td>
-                        <td class='emp-id'>607000</td>
-                        <td class='emp-name'>Abbas Savliwala</td>
-                        <td class='role'>
-                            <select>
-                                <option>User</option>
-                                <option>Transport Team</option>
-                                <option>Developer</option>
-                                <option>Admin</option>
-                            </select>
-                        </td>
-                        <td class='req-date'>08-08-2022</td>
-                        <td class='status'>
-                            <select>
-                                <option>Active</option>
-                                <option>Inactive</option>
-                                <option>Requested</option>
-                            </select>
-                        </td>
-                        <td class='confirm'><button class='btn btn-success'>Confirm</button></td>
-                        <td class='edit'><button class='btn btn-info'>Edit</button></td>
-                    </tr>
-                </tbody>
-            </table> */}
-        </div>
-    </>
-  )
+    // Added State to wait for JSON API
+    const [tableDataState, setTableDataState] = useState([]);
+    // Calling tableDataMethod to set state
+    tableDataMethod(setTableDataState);
+    return (
+        <>
+            <div class='container'>
+                <h4>Admin Page</h4>
+                {console.log(tableDataMethod, "tester")}
+                {/* Checks if the length of the JSON array is not 0. 
+                If 0 Display nothing, else display table */}
+                {tableDataState.length === 0 ? <></> : <Table tableData={tableDataState} />}
+
+            </div>
+        </>
+    )
 }
--- src/components/get_tableData.js
+++ src/components/get_tableData.js
@@ -3,20 +3,25 @@ const axios = require('axios');
 var tableData = [];
 var data = [];
 
-axios.get('http://localhost:3000/tableData')
-    .then(resp => {
-        data = resp.data;
-        data.forEach(e => {
-            tableData.push(e);
-            
-            console.log(`${e.id}, ${e.name}, ${e.status}`);
+// Setter function for TableDataState
+const tableDataMethod = (setter) => {
+    axios.get('http://localhost:3000/tableData')
+        .then(resp => {
+            data = resp.data;
+            data.forEach(e => {
+                tableData.push(e);
+
+                console.log(`${e.id}, ${e.name}, ${e.status}`);
+            });
+            // Once API call is complete and array is not empty,
+            // setter sets the state and sends tableData to Admin.js
+            setter(tableData)
+
+        })
+        .catch(error => {
+            console.log(error);
         });
-        
-    })
-    .catch(error => {
-        console.log(error);
-    });
 
-    export {tableData};
+};
+export default tableDataMethod;
 
-     
