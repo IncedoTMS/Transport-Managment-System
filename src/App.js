5d770a5ce69926e07af68ceeb59382e274c226d7
Gaurav Nirula <gaurav.nirula@incedoinc.com>
2022-08-09 13:05:56 +0530

    Formatted Code
    Redundant Code has been removed to make code more understandable and generic.

--- src/App.js
+++ src/App.js
@@ -9,30 +9,19 @@ function App() {
     <>
       <Router>
 
-        { /* These Buttons below are routing links to components */}
-        {/* <Link to="/">{<button type="button" class="btn btn-outline-primary">Dashboard</button>}</Link>
-        <Link to="/signin">{<button type="button" class="btn btn-outline-primary">Signin</button>}</Link>
-        <Link to="/signup">{<button type="button" class="btn btn-outline-primary">Signup</button>}</Link> */}
+        <Switch>
+          <Route exact path='/' component={Signin}>
+            <Signin />
+          </Route>
+          <Route path='/signup' component={Signup}>
+            <Signup />
+          </Route>
+          <Route path='/dashboard' component={Dashboard}>
 
-        {/* <Route exact path="/" component={Signin} /> */}
-        {/* <Route path="/signin" component={Dashboard} /> */}
-        {/* <Route path="/signup" component={Signup} /> */}
+          </Route>
+        </Switch>
 
-      
-
-      <Switch>
-      <Route exact path='/' component={Signin}>
-        <Signin/>
-      </Route>
-      <Route path='/signup' component={Signup}> 
-      <Signup/>                  
-      </Route>
-      <Route path='/dashboard' component={Dashboard}>
-
-      </Route>
-    </Switch>
-
-    </Router>
+      </Router>
 
     </>
   );
