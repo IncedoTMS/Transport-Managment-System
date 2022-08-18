f4731eb645bfb7fb8bf2a9590d47760d12c18cd0
adityashnkrW <110096852+adityashnkrW@users.noreply.github.com>
2022-08-18 11:17:18 +0530

    fixed folder structure issues

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Signin from "./Signin";
+// import Signin from "./Signin";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
