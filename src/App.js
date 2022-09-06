a38f8a0fe139abc2c19db7ee2f9bac7cf9ded911
Dinesh Nath Goswami <dinesh.goswami@incedoinc.com>
2022-09-06 13:56:33 +0530

    added 404 page, change header footer color, routing

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import UserDB from "./components/UserDB/UserDB";
 import Signup from "./components/Signup/Signup";
 import Signin from "./components/Signin/Signin";
 import Header from "./components/Common/Header";
 import Footer from "./components/Common/Footer";
+import NoMatch from "./components/Common/NoMatch";
 import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <div className="wrapper">
-        <header>
-          <Header />
-        </header>
-        <main>
-          <Switch>
-            <Route exact path="/" component={Signin}>
-              <Signin />
-            </Route>
-            <Route path="/signup" component={Signup}>
-              <Signup />
-            </Route>
-            <Route path="/dashboard" component={UserDB}></Route>
-          </Switch>
-        </main>
-        <footer>
-          <Footer />
-        </footer>
-      </div>
-    </Router>
+    <div className="wrapper">
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Signin}>
+            <Signin />
+          </Route>
+          <Route exact path="/signup" component={Signup}>
+            <Signup />
+          </Route>
+          <Route exact path="/dashboard" component={UserDB}></Route>
+          <Route path="*" component={NoMatch}></Route>
+        </Switch>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   );
 };
 
